65ece7c31971f38ea021295bc4d9a9954d56c748
mahmoudosama9966@gmail.com <mahmoudosama9966@gmail.com>
2023-02-19 02:40:45 +0200

    3

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,4 +3,9 @@ $(function () {
     $('.toggle-sidebar').on("click", function() {
         $(".content-area , .sidebar").toggleClass("no-sidebar")
     });
+    $(".toggle-submenu").on("click", function() {
+        $(this).find(".fa-sharp").toggleClass('down')
+        $(this).next(".child-links").slideToggle();
+    });
+
 });
